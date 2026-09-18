2f150d0d9acd50bae2715b84e4dddf8e7f934071
Will <hyghlyghter@icloud.com>
2026-09-17 22:44:16 -0500

    done

--- main.py
+++ main.py
@@ -19,24 +19,59 @@ class BinaryNumber:
 ## divide and conquer approach.
 
 def quadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
-
+    if isinstance(x, BinaryNumber):
+        x = x.binary_vec
+    if isinstance(y, BinaryNumber):
+        y = y.binary_vec
+    if len(x) == 1:
+        return int(x[0]) * int(y[0])
+    length = max(len(x), len(y))
+    size = 1
+    while size < length: #making lengths equal to a power of 2 for easier splitting
+        size *= 2
+    x = [0] * (size - len(x)) + x
+    y = [0] * (size - len(y)) + y 
+    xl = x[:len(x)//2]
+    xr = x[len(x)//2:]
+    yl = y[:len(y)//2]
+    yr = y[len(y)//2:]
+    p1 = quadratic_multiply(xl, yl)
+    p2 = quadratic_multiply(xl, yr)
+    p3 = quadratic_multiply(xr, yl)
+    p4 = quadratic_multiply(xr, yr)
+    return p1 * (2 ** len(x)) + (p2 + p3) * (2 ** (len(x) // 2)) + p4
+    
 def subquadratic_multiply(x, y):
-    ### TODO
-    pass
-    ###
+    if isinstance(x, BinaryNumber):
+        x = x.binary_vec
+    if isinstance(y, BinaryNumber):
+        y = y.binary_vec
+    if len(x) == 1:
+        return int(x[0]) * int(y[0])
+    length = max(len(x), len(y))
+    size = 1
+    while size < length: #making lengths equal to a power of 2 for easier splitting
+        size *= 2
+    x = ['0'] * (size - len(x)) + x
+    y = ['0'] * (size - len(y)) + y 
+    xl = x[:len(x)//2]
+    xr = x[len(x)//2:]
+    yl = y[:len(y)//2]
+    yr = y[len(y)//2:]
+    p1 = quadratic_multiply(xl, yl)
+    p2 = quadratic_multiply(xr, yr)
+    sum_x = BinaryNumber(int(''.join(xl), 2) + int(''.join(xr), 2))
+    sum_y = BinaryNumber(int(''.join(yl), 2) + int(''.join(yr), 2))
+    p3 = subquadratic_multiply(sum_x, sum_y)
+    return (p1 * (2 ** len(x))) + (p3 - p1 - p2) * (2 ** (len(x) // 2)) + p2
 
 def time_multiply(x, y, f):
     start = time.time()
-    # multiply two numbers x, y using function f
+    f(x, y)
     return (time.time() - start)*1000
     
-def compare_multiply():
-    pass
-    # compare the empirical runtimes of multiplication functions
-    ### TODO - add test cases and measure runtime
+def compare_multiply(x, y):
+    return time_multiply(x, y, quadratic_multiply(x, y) - subquadratic_multiply(x, y))
     
     
 
--- test_main.py
+++ test_main.py
@@ -7,7 +7,13 @@ from main import *
 # 5 pts
 def test_quadratic_multiply():
     assert quadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert quadratic_multiply(BinaryNumber(4), BinaryNumber(2)) == 4*2
+    assert quadratic_multiply(BinaryNumber(6), BinaryNumber(7)) == 6*7
+    assert quadratic_multiply(BinaryNumber(3), BinaryNumber(11)) == 3*11
 
 # 5 pts
 def test_subquadratic_multiply():
     assert subquadratic_multiply(BinaryNumber(2), BinaryNumber(2)) == 2*2
+    assert subquadratic_multiply(BinaryNumber(6), BinaryNumber(2)) == 6*2
+    assert subquadratic_multiply(BinaryNumber(9), BinaryNumber(3)) == 9*3
+    assert subquadratic_multiply(BinaryNumber(7), BinaryNumber(4)) == 7*4
